e2a2d134bee4abd368c60d98b37d069275ffc562
prudhvi <prudhvidandamudi@gmail.com>
2025-01-26 21:45:52 -0600

    updating .gitignore

--- test-automation/tests/ui/example.spec.ts
+++ test-automation/tests/ui/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('./');
+  await page.goto('.');
 
   await page.getByRole('link', { name: 'Signup / Login' }).click();
   // Expect a title "to contain" a substring.
